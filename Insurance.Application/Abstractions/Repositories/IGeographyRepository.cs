62e0bb37b264fa2258beeb123f4e2eb4c4b7148c
Tudor Leonte <tudor.leonte10@gmail.com>
2026-01-16 13:39:05 +0200

    added repository for geography

---
+++ Insurance.Application/Abstractions/Repositories/IGeographyRepository.cs
@@ -0,0 +1,14 @@
+﻿using Insurance.Domain.Geography;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Insurance.Application.Abstractions.Repositories
+{
+    public interface IGeographyRepository
+    {
+        Task<IReadOnlyList<Country>> GetAllCountriesAsync(CancellationToken cancellationToken);
+        Task<IReadOnlyList<County>> GetCountiesByCountryIdAsync(Guid countryId, CancellationToken cancellationToken);
+        Task<IReadOnlyList<City>> GetCitiesByCountyIdAsync(Guid countyId, CancellationToken cancellationToken);
+    }
+}
---
+++ Insurance.Infrastructure/Persistence/Repositories/GeographyRepository.cs
@@ -0,0 +1,45 @@
+﻿using Insurance.Application.Abstractions.Repositories;
+using Insurance.Domain.Geography;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Insurance.Infrastructure.Persistence.Repositories
+{
+    public class GeographyRepository : IGeographyRepository
+    {
+        private readonly InsuranceDbContext _context;
+
+        public GeographyRepository(InsuranceDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IReadOnlyList<Country>> GetAllCountriesAsync(CancellationToken cancellationToken)
+        {
+            return await _context.Countries
+                .AsNoTracking()
+                .OrderBy(c => c.Name)
+                .ToListAsync(cancellationToken);
+        }
+
+        public async Task<IReadOnlyList<City>> GetCitiesByCountyIdAsync(Guid countyId, CancellationToken cancellationToken)
+        {
+            return await _context.Cities
+                .AsNoTracking()
+                .Where(c => c.CountyId == countyId)
+                .OrderBy(c => c.Name)
+                .ToListAsync(cancellationToken);
+        }
+
+        public async Task<IReadOnlyList<County>> GetCountiesByCountryIdAsync(Guid countryId, CancellationToken cancellationToken)
+        {
+            return await _context.Counties
+                .AsNoTracking()
+                .Where(c => c.CountryId == countryId)
+                .OrderBy(c => c.Name)
+                .ToListAsync(cancellationToken);
+        }
+    }
+}
