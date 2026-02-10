791904475674cd62dbdd6c41933da0a33c89d0e7
Tudor Leonte <tudor.leonte10@gmail.com>
2026-02-10 09:01:57 +0200

    fixed swagger auth

--- Insurance.WebApi/Controllers/PolicyController.cs
+++ Insurance.WebApi/Controllers/PolicyController.cs
@@ -10,7 +10,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace Insurance.WebApi.Controllers
 {
     [ApiController]
-   //[Authorize(Roles = "Broker")]
+   [Authorize(Roles = "Broker")]
     [Route("api/brokers/policies")]
     public class PolicyController : ControllerBase
     {
--- Insurance.WebApi/Program.cs
+++ Insurance.WebApi/Program.cs
@@ -4,6 +4,9 @@ using Insurance.Infrastructure.Persistence;
 using Insurance.Infrastructure.Persistence.Seed;
 using Insurance.WebApi.Middleware;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi;
+using System.Reflection.Metadata;
 using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -22,7 +25,27 @@ if (!builder.Environment.IsEnvironment("Test"))
 }
 
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo 
+    { 
+        Title = "Insurance API", 
+        Version = "v1" 
+    });
+
+    c.AddSecurityDefinition("bearer", new OpenApiSecurityScheme
+    {
+        Type = SecuritySchemeType.Http,
+        Scheme = "bearer",
+        BearerFormat = "JWT",
+        Description = "JWT Authorization header using the Bearer scheme."
+    });
+
+    c.AddSecurityRequirement(doc => new OpenApiSecurityRequirement
+    {
+        [new OpenApiSecuritySchemeReference("bearer", doc)] = []
+    });
+});
 
 var app = builder.Build();
 
@@ -54,8 +77,8 @@ app.UseMiddleware<ExceptionHandlingMiddleware>();
 
 app.UseHttpsRedirection();
 
-//app.UseAuthentication();
-//app.UseAuthorization();
+app.UseAuthentication();
+app.UseAuthorization();
 
 app.MapControllers();
 
